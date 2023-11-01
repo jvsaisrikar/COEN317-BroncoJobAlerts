cc68088cd4158476e9235bb5478992d48721cb45
srikar <jvsaisrikar@gmail.com>
2023-11-01 12:55:53 -0700

    Initial Commit BoilterPlate
    Signed-off-by: srikar <jvsaisrikar@gmail.com>

--- producer.py
+++ producer.py
@@ -5,6 +5,7 @@ credentials = pika.PlainCredentials('guest', 'guest')
 connection_parameters = pika.ConnectionParameters('localhost', port=5673, credentials=credentials)
 
 connection = pika.BlockingConnection(connection_parameters)
+<<<<<<< HEAD
 channel = connection.channel()
 # make exchange durable
 channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct, durable=True)
@@ -20,4 +21,18 @@ channel.basic_publish(exchange='routing', routing_key='external', body="message 
 channel.basic_publish(exchange='routing', routing_key='all', body="Both internal and external should get this message **", properties=pika.BasicProperties(delivery_mode=2))
 
 print(f'sent message: {message}')
+=======
+
+channel = connection.channel()
+
+channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct)
+
+message = 'This message needs to be routed'
+
+channel.basic_publish(exchange='routing', routing_key='external', body="message from extenal topic")
+channel.basic_publish(exchange='routing', routing_key='both', body="Both internal and external should get this message")
+
+print(f'sent message: {message}')
+
+>>>>>>> fce6047 (Initial Commit BoilterPlate)
 connection.close()
