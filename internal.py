c1a7842266f13dd52334912abcfa49edc24c2da6
srikar <jvsaisrikar@gmail.com>
2023-11-06 14:56:35 -0800

    Added Consumer api's sub, unsub, get
    Signed-off-by: srikar <jvsaisrikar@gmail.com>

--- internal.py
+++ internal.py
@@ -1,26 +1,93 @@
+from flask import Flask, request, jsonify
 import pika
-from pika.exchange_type import ExchangeType
 
-def on_message_received(ch, method, properties, body):
-    print(f'Internal - received new message: {body}')
+app = Flask(__name__)
 
+# RabbitMQ setup parameters
 credentials = pika.PlainCredentials('guest', 'guest')
-connection_parameters = pika.ConnectionParameters('localhost', port=5673, credentials=credentials)
+connection_parameters = pika.ConnectionParameters(
+    host='localhost',
+    port=5673,
+    credentials=credentials,
+    heartbeat=10
+)
 
-connection = pika.BlockingConnection(connection_parameters)
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    connection = pika.BlockingConnection(connection_parameters)
+    channel = connection.channel()
 
-channel = connection.channel()
+    data = request.json
+    username = data.get('username')
+    topic = data.get('topic')
+    if not username or not topic:
+        channel.close()
+        connection.close()
+        return jsonify({'error': 'Missing username or topic'}), 400
 
-channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct)
+    queue_name = username
+    # Declare a durable, non-exclusive queue
+    queue = channel.queue_declare(queue=queue_name, durable=True, exclusive=False)
+    channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key=topic)
 
-queue = channel.queue_declare(queue='', exclusive=True)
+    channel.close()
+    connection.close()
+    return jsonify({'status': 'subscribed', 'queue': queue_name, 'topic': topic}), 200
 
-channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key='internal')
-channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key='both')
+@app.route('/unsubscribe', methods=['POST'])
+def unsubscribe():
+    connection = pika.BlockingConnection(connection_parameters)
+    channel = connection.channel()
 
-channel.basic_consume(queue=queue.method.queue, auto_ack=True,
-    on_message_callback=on_message_received)
+    data = request.json
+    username = data.get('username')
+    topic = data.get('topic')
+    if not username or not topic:
+        channel.close()
+        connection.close()
+        return jsonify({'error': 'Missing username or topic'}), 400
 
-print('Internal Starting Consuming')
+    queue_name = username
+    #just unbinding not deleting the queue, can be used for other topics
+    channel.queue_unbind(exchange='routing', queue=queue_name, routing_key=topic)
 
-channel.start_consuming()
+
+    channel.close()
+    connection.close()
+    return jsonify({'status': 'unsubscribed', 'queue': queue_name, 'topic': topic}), 200
+
+@app.route('/get_message', methods=['GET'])
+def get_message():
+    connection = pika.BlockingConnection(connection_parameters)
+    channel = connection.channel()
+
+    username = request.args.get('username')
+    if not username:
+        channel.close()
+        connection.close()
+        return jsonify({'error': 'Missing username parameter'}), 400
+
+    queue_name = username
+
+    try:
+        method_frame, header_frame, body = channel.basic_get(queue=queue_name, auto_ack=True)
+        if method_frame:
+            message = body.decode('utf-8')
+            response = jsonify({'status': 'success', 'message': message}), 200
+        else:
+            response = jsonify({'status': 'empty', 'message': 'No message available'}), 200
+    except pika.exceptions.ChannelClosedByBroker as e:
+        # case where the queue does not exist
+        response = jsonify({'error': str(e)}), 404
+    except Exception as e:
+        # exceptions
+        response = jsonify({'error': 'An error occurred'}), 500
+    finally:
+        channel.close()
+        connection.close()
+
+    return response
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- producer.py
+++ producer.py
@@ -5,19 +5,14 @@ credentials = pika.PlainCredentials('guest', 'guest')
 connection_parameters = pika.ConnectionParameters('localhost', port=5673, credentials=credentials)
 
 connection = pika.BlockingConnection(connection_parameters)
-
 channel = connection.channel()
-
 channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct)
-
 message = 'This message needs to be routed'
 
-
+# sample hardcoded messages
 channel.basic_publish(exchange='routing', routing_key='internal', body="message from internal topic")
 channel.basic_publish(exchange='routing', routing_key='external', body="message from extenal topic")
-channel.basic_publish(exchange='routing', routing_key='both', body="Both internal and external should get this message")
-
+channel.basic_publish(exchange='routing', routing_key='all', body="Both internal and external should get this message")
 
 print(f'sent message: {message}')
-
 connection.close()
