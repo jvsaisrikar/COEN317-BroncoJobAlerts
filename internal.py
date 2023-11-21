be596257e89f3b68a7a990938c19285eab690480
srikar <jvsaisrikar@gmail.com>
2023-11-21 14:58:31 -0800

    Concurrency and Mutual Exclustion First Cut
    Signed-off-by: srikar <jvsaisrikar@gmail.com>

--- internal.py
+++ internal.py
@@ -1,5 +1,27 @@
 from flask import Flask, request, jsonify
 import pika
+from threading import Thread
+import logging
+import time
+import requests
+from requests.auth import HTTPBasicAuth
+from threading import Lock
+
+# Lock declaration
+subscribe_lock = Lock()
+
+def fetch_queues():
+    url = "http://localhost:15673/api/queues"
+    response = requests.get(url, auth=HTTPBasicAuth('guest', 'guest'))
+    if response.status_code == 200:
+        queues = response.json()
+        return [q['name'] for q in queues if q['name']]  # Filter out any queues without names
+    else:
+        logging.error(f"Failed to fetch queues: {response.text}")
+        return []
+
+# Configure logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = Flask(__name__)
 
@@ -12,82 +34,103 @@ connection_parameters = pika.ConnectionParameters(
     heartbeat=10
 )
 
+# Dictionary to store consumer threads
+consumer_threads = {}
+
+def message_consumer(queue_name):
+    while True:  # Reconnection loop
+        try:
+            connection = pika.BlockingConnection(connection_parameters)
+            channel = connection.channel()
+
+            channel.queue_declare(queue=queue_name, durable=True)
+            channel.basic_qos(prefetch_count=1)
+
+            def callback(ch, method, properties, body):
+                message = body.decode()
+                logging.info(f'Received message in {queue_name}: {message}')
+                ch.basic_ack(delivery_tag=method.delivery_tag)
+
+            channel.basic_consume(queue=queue_name, on_message_callback=callback)
+            logging.info(f'Starting message consumption for {queue_name}')
+            channel.start_consuming()
+        except pika.exceptions.AMQPConnectionError as e:
+            logging.error(f'Connection was closed, retrying in 5 seconds: {e}')
+            time.sleep(5)
+        except Exception as e:
+            logging.error(f'Unexpected error: {e}, exiting consumer thread for {queue_name}')
+            break  # Exit the thread if an unexpected error occurs
+        finally:
+            if connection.is_open:
+                connection.close()
 @app.route('/subscribe', methods=['POST'])
 def subscribe():
-    connection = pika.BlockingConnection(connection_parameters)
-    channel = connection.channel()
+    try:
+        data = request.json
+        username = data.get('username')
+        topic = data.get('topic')
+        if not username or not topic:
+            return jsonify({'error': 'Missing username or topic'}), 400
 
-    data = request.json
-    username = data.get('username')
-    topic = data.get('topic')
-    if not username or not topic:
+        connection = pika.BlockingConnection(connection_parameters)
+        channel = connection.channel()
+
+        queue_name = username
+        channel.queue_declare(queue=queue_name, durable=True, exclusive=False)
+
+        # locking here when binding; unlocking will be done after this execution.
+        with subscribe_lock:
+            channel.queue_bind(exchange='routing', queue=queue_name, routing_key=topic)
+
         channel.close()
         connection.close()
-        return jsonify({'error': 'Missing username or topic'}), 400
 
-    queue_name = username
-    # Declare a durable, non-exclusive queue
-    queue = channel.queue_declare(queue=queue_name, durable=True, exclusive=False)
-    channel.queue_bind(exchange='routing', queue=queue.method.queue, routing_key=topic)
+        if username not in consumer_threads:
+            consumer_thread = Thread(target=message_consumer, args=(username,))
+            consumer_threads[username] = consumer_thread
+            consumer_thread.start()
 
-    channel.close()
-    connection.close()
-    return jsonify({'status': 'subscribed', 'queue': queue_name, 'topic': topic}), 200
+        return jsonify({'status': 'subscribed', 'queue': queue_name, 'topic': topic}), 200
+    except Exception as e:
+        logging.error(f"Subscription error: {e}")
+        return jsonify({'error': 'Failed to subscribe', 'details': str(e)}), 500
+
+
 
 @app.route('/unsubscribe', methods=['POST'])
 def unsubscribe():
-    connection = pika.BlockingConnection(connection_parameters)
-    channel = connection.channel()
-
     data = request.json
     username = data.get('username')
     topic = data.get('topic')
+
     if not username or not topic:
-        channel.close()
-        connection.close()
         return jsonify({'error': 'Missing username or topic'}), 400
 
-    queue_name = username
-    #just unbinding not deleting the queue, can be used for other topics
-    channel.queue_unbind(exchange='routing', queue=queue_name, routing_key=topic)
-
-
-    channel.close()
-    connection.close()
-    return jsonify({'status': 'unsubscribed', 'queue': queue_name, 'topic': topic}), 200
-
-@app.route('/get_message', methods=['GET'])
-def get_message():
-    connection = pika.BlockingConnection(connection_parameters)
-    channel = connection.channel()
-
-    username = request.args.get('username')
-    if not username:
-        channel.close()
-        connection.close()
-        return jsonify({'error': 'Missing username parameter'}), 400
-
-    queue_name = username
-
     try:
-        method_frame, header_frame, body = channel.basic_get(queue=queue_name, auto_ack=True)
-        if method_frame:
-            message = body.decode('utf-8')
-            response = jsonify({'status': 'success', 'message': message}), 200
-        else:
-            response = jsonify({'status': 'empty', 'message': 'No message available'}), 200
-    except pika.exceptions.ChannelClosedByBroker as e:
-        # case where the queue does not exist
-        response = jsonify({'error': str(e)}), 404
-    except Exception as e:
-        # exceptions
-        response = jsonify({'error': 'An error occurred'}), 500
-    finally:
+        connection = pika.BlockingConnection(connection_parameters)
+        channel = connection.channel()
+
+        # Unbind the topic from the user's queue
+        # locking here when unbinding; unlocking will be done after this execution.
+        with subscribe_lock:
+            channel.queue_unbind(queue=username, exchange='routing', routing_key=topic)
+
         channel.close()
         connection.close()
+        return jsonify({'status': 'unsubscribed', 'queue': username, 'topic': topic,
+                        'message': 'Topic unbound from queue successfully'}), 200
+    except Exception as e:
+        return jsonify({'error': 'Failed to unbind topic from queue', 'details': str(e)}), 500
 
-    return response
-
+def start_consumers_on_startup():
+    queue_names = fetch_queues()
+    for queue_name in queue_names:
+        if queue_name not in consumer_threads:
+            consumer_thread = Thread(target=message_consumer, args=(queue_name,))
+            consumer_threads[queue_name] = consumer_thread
+            consumer_thread.start()
+            logging.info(f"Started consumer for {queue_name}")
 
 if __name__ == '__main__':
+    start_consumers_on_startup()
     app.run(debug=True)
--- producer.py
+++ producer.py
@@ -6,13 +6,18 @@ connection_parameters = pika.ConnectionParameters('localhost', port=5673, creden
 
 connection = pika.BlockingConnection(connection_parameters)
 channel = connection.channel()
-channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct)
+# make exchange durable
+channel.exchange_declare(exchange='routing', exchange_type=ExchangeType.direct, durable=True)
 message = 'This message needs to be routed'
 
 # sample hardcoded messages
-channel.basic_publish(exchange='routing', routing_key='internal', body="message from internal topic")
-channel.basic_publish(exchange='routing', routing_key='external', body="message from extenal topic")
-channel.basic_publish(exchange='routing', routing_key='all', body="Both internal and external should get this message")
+# mark messages as persistent - by setting delivery_mode property = 2
+channel.basic_publish(exchange='routing', routing_key='internal', body="message from internal topic **", properties=pika.BasicProperties(delivery_mode=2))
+channel.basic_publish(exchange='routing', routing_key='external', body="message from extenal topic 1 **", properties=pika.BasicProperties(delivery_mode=2))
+channel.basic_publish(exchange='routing', routing_key='external', body="message from extenal topic 2 **", properties=pika.BasicProperties(delivery_mode=2))
+channel.basic_publish(exchange='routing', routing_key='external', body="message from extenal topic 3 **", properties=pika.BasicProperties(delivery_mode=2))
+channel.basic_publish(exchange='routing', routing_key='external', body="message from extenal topic 4 **", properties=pika.BasicProperties(delivery_mode=2))
+channel.basic_publish(exchange='routing', routing_key='all', body="Both internal and external should get this message **", properties=pika.BasicProperties(delivery_mode=2))
 
 print(f'sent message: {message}')
 connection.close()
